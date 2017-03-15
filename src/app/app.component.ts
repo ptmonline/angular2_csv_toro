02ef471fa905f48fdc1bae12dc9a022c00a6870f
Pau Torres <CE9@costaisa.org>
2017-03-15 17:11:10 +0100

    initial build

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1>`,
+})
+export class AppComponent  { title = 'Toro Angular2 CSV test'; }
