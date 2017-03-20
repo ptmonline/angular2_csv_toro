d0e12ff00037c977421c37d462ff59cb15a3cd16
Pau Torres <ptmonline@users.noreply.github.com>
2017-03-20 09:17:55 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,6 @@
 var gulp = require("gulp");
-// var less = require("gulp-less");
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
-
-// gulp.task('default', function () {
-//   gulp.src('src/less/*.less')
-//     .pipe(less())
-//     .pipe(rename({
-//       suffix: '.min'
-//     }))
-//     .pipe(cleanCSS({ debug: true }, function (details) {
-//       console.log(details.name + ': ' + details.stats.originalSize);
-//       console.log(details.name + ': ' + details.stats.minifiedSize);
-//     }))
-//     .pipe(gulp.dest('src/css'));
-// })
-
-// gulp.task('watch', function () {
-//   gulp.watch('src/less/*.less', ['default'])
-// })
-
-var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
